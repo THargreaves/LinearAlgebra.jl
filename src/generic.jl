0a253be373ecaa4639ca27c7558e598addb79806
Jishnu Bhattacharya <jishnub.github@gmail.com>
2025-04-07 19:20:30 +0530

    Explicit loop in `_iszero` for strided views (#1264)
    This reduces TTFX
    ```julia
    julia> using LinearAlgebra
    
    julia> Z = zeros(4,4);
    
    julia> @time istril(Z);
      0.152632 seconds (572.99 k allocations: 27.476 MiB, 99.98% compilation time) # master
      0.091427 seconds (177.39 k allocations: 8.688 MiB, 99.97% compilation time) # this PR
    ```

--- src/generic.jl
+++ src/generic.jl
@@ -1358,8 +1358,15 @@ ishermitian(x::Number) = (x == conj(x))
 _iszero(V) = iszero(V)
 # A Base.FastContiguousSubArray view of a StridedArray
 FastContiguousSubArrayStrided{T,N,P<:StridedArray,I<:Tuple{AbstractUnitRange, Vararg{Any}}} = Base.SubArray{T,N,P,I,true}
-# using mapreduce instead of all permits vectorization
-_iszero(V::FastContiguousSubArrayStrided) = mapreduce(iszero, &, V, init=true)
+# Reducing over the entire array instead of calling `all` within `iszero` permits vectorization
+# The loop is equivalent to a mapreduce, but is faster to compile
+function _iszero(V::FastContiguousSubArrayStrided)
+    ret = true
+    for i in eachindex(V)
+        ret &= iszero(@inbounds V[i])
+    end
+    ret
+end
 
 """
     istriu(A::AbstractMatrix, k::Integer = 0) -> Bool
