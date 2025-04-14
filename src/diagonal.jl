222f7f29e9d8ac26d286494503698707136bc69d
Jishnu Bhattacharya <jishnub.github@gmail.com>
2025-04-14 14:18:56 +0530

    Propagate inbounds in diagzero (#1285)

--- src/diagonal.jl
+++ src/diagonal.jl
@@ -191,7 +191,7 @@ end
 Return the appropriate zero element `A[i, j]` corresponding to a banded matrix `A`.
 """
 diagzero(A::AbstractMatrix, i, j) = zero(eltype(A))
-diagzero(A::AbstractMatrix{M}, i, j) where {M<:AbstractMatrix} =
+@propagate_inbounds diagzero(A::AbstractMatrix{M}, i, j) where {M<:AbstractMatrix} =
     zeroslike(M, axes(A[i,i], 1), axes(A[j,j], 2))
 diagzero(A::AbstractMatrix, inds...) = diagzero(A, to_indices(A, inds)...)
 # dispatching on the axes permits specializing on the axis types to return something other than an Array
